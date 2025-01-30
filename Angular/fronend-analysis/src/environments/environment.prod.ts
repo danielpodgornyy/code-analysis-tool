39aac44577adc7e5430018ea954bff74854111b3
Fnu Abdullah <abdullahmkh004@gmail.com>
2025-01-29 22:35:09 -0600

    Environments folder for initializing Firebase App

---
+++ Angular/fronend-analysis/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBy9ohipSmrQvI_7QRJvxKHi0Uy0wtWz00",
+    authDomain: "code-analysis-a27ff.firebaseapp.com",
+    projectId: "code-analysis-a27ff",
+    storageBucket: "code-analysis-a27ff.firebasestorage.app",
+    messagingSenderId: "1020108267358",
+    appId: "1:1020108267358:web:e1f017673c4be2e64939cf"
+};
+
+export const environment = {
+    production: true,
+    firebase: firebaseConfig
+};
