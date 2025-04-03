3584aed80e30f200a32408d607c63cd6c4d1e5e5
Fnu Abdullah <abdullahmkh004@gmail.com>
2025-04-03 09:22:54 -0500

    filename/:filename server prerending

--- frontend/fronend-analysis/src/app/app.routes.server.ts
+++ frontend/fronend-analysis/src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'file-details/:filename',
+    renderMode: RenderMode.Server  // Disable prerendering for this route
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
